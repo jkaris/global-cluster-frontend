e50a47e1973021445783fe543faff377fbd5ebea
Mohamed-code-marvel <mohaabdul568@gmail.com>
2024-07-30 05:41:29 +0300

    Removed unused imports

--- src/components/BusinessSignUp.jsx
+++ src/components/BusinessSignUp.jsx
@@ -2,17 +2,14 @@ import React, { useState } from "react";
 
 import LoginPageImg from "./../assets/images/loginPageSideImg.jpeg";
 import BusinessSignUpForm from "./BusinessSignUpForm";
-import ComapnySizeInput from "./ComapnySizeInput";
 import LoginHeader from "./LoginHeader";
 
 function BusinessSignUp() {
   const [companySizeInputShow, SetCompanySizeInputShow] = useState(true);
-  const [companySize, setCompanySize] = useState();
   return (
     <div className="min-h-screen flex flex-col">
       <LoginHeader />
       <div className="flex flex-1 justify-between ">
-        {/* Left side - Login Form */}
         <div className="w-full flex flex-col p-8 ">
           <div className="flex-grow flex flex-col  justify-center items-center">
             <div className="mt-20 w-[50%] flex flex-col gap-2">
@@ -23,17 +20,6 @@ function BusinessSignUp() {
               >
                 Get Started with Global Cluster as a Business
               </h2>
-              {/* {companySizeInputShow ? (
-                <BusinessSignUpForm
-                  companySizeInput={companySizeInputShow}
-                  SetCompanySizeInput={SetCompanySizeInputShow}
-                />
-              ) : (
-                <ComapnySizeInput
-                  companySize={companySize}
-                  setCompanySize={setCompanySize}
-                />
-              )} */}
               <BusinessSignUpForm
                 companySizeInput={companySizeInputShow}
                 SetCompanySizeInput={SetCompanySizeInputShow}
@@ -41,8 +27,6 @@ function BusinessSignUp() {
             </div>
           </div>
         </div>
-
-        {/* Right side - Image with Gradient Overlay */}
         <div className="hidden lg:block w-8/12 relative">
           <div className="absolute inset-0 bg-gradient-to-b  from-gray-500 to-gray-600 opacity-70"></div>
           <img
